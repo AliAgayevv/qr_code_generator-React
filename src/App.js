a35d8bdc7bd0f24a870f353b893597bc0671d92d
AliAgayevv <aghayev_233@mail.ru>
2024-10-29 12:51:32 +0400

    codes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import QRCodeGenerator from "./Components/qr-code-generator";
+
+export default function App() {
+  return (
+    <div>
+        <QRCodeGenerator />
+
+    </div>
+  );
+}
+
